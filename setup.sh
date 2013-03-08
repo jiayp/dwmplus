b1fc89186cd7ab03cf466790735ed4392f5ae870
jingyuli <jingyuli@rdamicro.com>
2013-03-08 20:13:13 +0800

    change vim_replace.sh path

--- setup.sh
+++ setup.sh
@@ -79,27 +79,6 @@ function cscleana()
     fi
 }
 
-function vim_replace()
-{
-    if [[ $# != 3 ]];then
-        return
-    fi
-
-    pattern=$2
-    rep_pattern=$3
-    filelist=`cat $1`
-    repeatfile=""
-    for arg in $filelist
-    do
-        if [[ $arg != $repeatfile ]];then
-            sed "s/$pattern/$rep_pattern/" $arg  > ~/.gvim_repfile
-            rm $arg 
-            cp -rf ~/.gvim_repfile $arg
-            rm  ~/.gvim_repfile
-        fi
-        repeatfile=$arg
-    done
-}
 function ll()
 {
     ls -l $*
--- vim_replace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. ~/bin/setup.sh
-vim_replace $@
